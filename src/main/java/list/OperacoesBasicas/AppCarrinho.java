28680a624b341dfed8efe4cc39a45de126d89dc6
Paulo Jonas <pjonas21@gmail.com>
2023-09-01 16:39:53 -0300

    alteracao arquivo - segunda parte - exercio

---
+++ src/main/java/list/OperacoesBasicas/AppCarrinho.java
@@ -0,0 +1,23 @@
+package main.java.list.OperacoesBasicas;
+
+public class AppCarrinho {
+    public static void main(String[] args) {
+        CarrinhoCompras carrinho = new CarrinhoCompras();
+
+        carrinho.adicionarItem("Caneta", 1.49, 5);
+        carrinho.adicionarItem("Caderno", 10.49, 2);
+        carrinho.adicionarItem("Mochila", 149.49, 1);
+
+        carrinho.exibirItens();
+        System.out.println("Valor total do carrinho de compras: R$ " + carrinho.calcularValorTotal());
+
+        carrinho.removerItem("Caneta");
+        carrinho.exibirItens();
+        System.out.println("Valor total do carrinho de compras: R$ " + carrinho.calcularValorTotal());
+
+        carrinho.alterarItem(1, "Caneta", 1, 1.59);
+        carrinho.exibirItens();
+        System.out.println("Valor total do carrinho de compras alterado: R$ " + carrinho.calcularValorTotal());
+
+    }
+}
