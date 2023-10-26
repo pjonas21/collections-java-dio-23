3eb8d82540bb296f32d13cbf03ef929dae5e7de9
Paulo Jonas <pjonas21@gmail.com>
2023-10-26 16:44:35 -0300

    aula SET

---
+++ src/main/java/set/opbasicas/ConjuntoConvidados.java
@@ -0,0 +1,49 @@
+package main.java.set.opbasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void addConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite) throws Exception {
+        Convidado convidadoParaRemover = null;
+        for (Convidado c: convidadoSet
+             ) {
+            if (c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            } else {
+                throw new Exception("Não existe convidado com esse código de convite.");
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados.");
+
+        conjuntoConvidados.addConvidado("Jonas", 1234);
+        conjuntoConvidados.addConvidado("Vanessa", 1235);
+        conjuntoConvidados.addConvidado("Janio", 1236);
+        conjuntoConvidados.addConvidado("Juliana", 1237);
+    }
+}
